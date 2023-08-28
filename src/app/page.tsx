986951b8c26a6d9a246a531f10cfb99999fdf2c9
agusgonz <agustingendeiza@gmail.com>
2023-08-28 14:55:34 -0300

    Add basic weather display section
    It only includes the CurrentTemp PrecipProb WindSpeed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import WeatherSection from "../components/WeatherSection/index"
+
 export default function Home() {
-	return <></>
+	return (
+		<>
+			<WeatherSection />
+		</>
+	)
 }
---
+++ src/components/WeatherSection/NowSection/index.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react"
+import PrecipProbComp from "./PrecipProbComp"
+import CurrentTempComp from "./CurrentTempComp"
+import WindSpeedComp from "./WindSpeedComp"
+import AdjustWindowSize from "../AdjustWindowSize"
+
+interface NowSectionProps {}
+
+const NowSection: FC<NowSectionProps> = ({}) => {
+	return (
+		<AdjustWindowSize>
+			<div className="flex flex-row gap-16 justify-center items-center w-screen">
+				{/* Precipitation Probability */}
+				<PrecipProbComp hiddeOnMobile />
+
+				{/* Current Temperature */}
+				<CurrentTempComp />
+
+				{/* Wind Speed */}
+				<WindSpeedComp hiddeOnMobile />
+			</div>
+		</AdjustWindowSize>
+	)
+}
+export default NowSection
