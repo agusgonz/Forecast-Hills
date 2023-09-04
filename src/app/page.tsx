f4de62463b5ca116d301edf73f81b995b54f3eb7
agusgonz <agustingendeiza@gmail.com>
2023-09-04 11:11:11 -0300

    Move weather and location fetch to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,56 @@
 "use client"
-import BackgroundVideo from "@/components/BackgroundVideoFolder/BackgroundVideo"
-import WeatherSection from "@/components/WeatherSectionFolder/WeatherSection"
+import BackgroundVideo from "@/components/BackgroundVideo"
+import Navbar from "@/components/Navbar"
+import WeatherSection from "@/components/WeatherSection"
 import { useAppDispatch } from "@/hooks/redux/useAppDispatch"
-import { useAppSelector } from "@/hooks/redux/useAppSelector"
-import { toggleOpenWeatherSection } from "@/libs/redux/slices/openWeatherSection"
-import { useState } from "react"
+import { setLocationData } from "@/libs/redux/slices/locationData"
+import { setWeatherData } from "@/libs/redux/slices/weatherData"
+import { getCity } from "@/utils/services/open-cage/getCity"
+import { getWeather } from "@/utils/services/open-meteo/getWeather"
+import { useEffect } from "react"
 
 export default function Home() {
-	const open = useAppSelector(
-		state => state.openWeatherSection.value
-	)
+	const dispatch = useAppDispatch()
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			positionSuccess,
+			positionError
+		)
+
+		// if it was successful
+		async function positionSuccess(
+			coords: GeolocationPosition
+		) {
+			// get weather from api
+			const data = await getWeather(
+				coords.coords.latitude,
+				coords.coords.longitude
+			)
+			dispatch(setWeatherData(data))
+
+			// get city from api
+			const city = await getCity(
+				coords.coords.latitude,
+				coords.coords.longitude
+			)
+			dispatch(setLocationData({ value: city }))
+		}
+
+		// if there was an error
+		function positionError() {
+			alert("There was an error getting your location")
+		}
+	}, [])
 
 	return (
 		<div
-			className="relative h-screen bg-black
+			className="relative h-screen
 		"
 		>
-			<div
-				className={`${
-					open ? "h-[30vh]" : "h-[76vh]"
-				} transition-all duration-300`}
-			>
-				<BackgroundVideo />
-			</div>
-			<div
-				className={`${
-					open ? "h-[70vh]" : "h-[24vh]"
-				} w-full  min-h-[200px] absolute bottom-0 transition-all duration-300`}
-			>
-				<WeatherSection />
-			</div>
+			<Navbar />
+			<BackgroundVideo />
+			<WeatherSection />
 		</div>
 	)
 }
