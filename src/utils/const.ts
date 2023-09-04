384d4d91c7a77e9caefc395d343627bd06d68e92
agusgonz <agustingendeiza@gmail.com>
2023-09-04 18:42:26 -0300

    Add theme constants

---
+++ src/utils/const.ts
@@ -0,0 +1,22 @@
+export const Themes = [
+	{
+		name: "day-1",
+		primaryColor: "#135FA0",
+		seconaryColor: "#4089c3",
+	},
+	{
+		name: "day-2",
+		primaryColor: "#492C24",
+		seconaryColor: "#492C24",
+	},
+	{
+		name: "night-1",
+		primaryColor: "#011939",
+		seconaryColor: "#1a468d",
+	},
+	{
+		name: "night-2",
+		primaryColor: "#081332",
+		seconaryColor: "#440b2f",
+	},
+]
